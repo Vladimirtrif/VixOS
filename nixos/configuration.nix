a110e4f767d14e6d65640fc8f1a9629f63cfbd58
Vladimirtrif <vova.trifonov@hotmail.com>
2024-09-09 06:57:01 -0700

    Reverted commit, removed from www group since it didn't fix github sudo issue

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -65,7 +65,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
    users.users.vova = {
      isNormalUser = true;
-     extraGroups = [ "wheel" "docker" "vboxsf" "www-data" ]; # Enable ‘sudo’ for the user.
+     extraGroups = [ "wheel" "docker" "vboxsf"]; # Enable ‘sudo’ for the user.
      home = "/home/vova";
      packages = with pkgs; [
       # firefox
