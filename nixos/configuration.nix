c863b7d2a655f54b7a17dff5d8936c56e6c32d32
Vladimirtrif <59485365+Vladimirtrif@users.noreply.github.com>
2024-09-11 21:50:10 +0300

    Refactor bootloader

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,25 +8,19 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-     # "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
       ../disko.nix
       ./packages.nix
       ./modules/bundle.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
- # boot.loader.systemd-boot.enable = true;
-  #boot.loader.efi.canTouchEfiVariables = true;
-    boot.loader.grub.enable = true;
-    boot.loader.grub.efiSupport = true;
-    boot.loader.grub.efiInstallAsRemovable = true;
-
+    #enable flakes
     nix.settings.experimental-features = ["nix-command" "flakes"];
 
-    networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-    networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+    # Define your hostname.
+    networking.hostName = "nixos";
+
+    # Easiest to use and most distros use this by default.
+    networking.networkmanager.enable = true;
 
   # Set your time zone.
     time.timeZone = "America/Los_Angeles";
@@ -37,22 +31,16 @@
 
   # Select internationalisation properties.
    i18n.defaultLocale = "en_US.UTF-8";
+
    console = {
      font = "Lat2-Terminus16";
      keyMap = "us";
-    # useXkbConfig = true; # use xkb.options in tty.
    };
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
 
   # Enable CUPS to print documents.
    services.printing.enable = true;
 
   # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
    services.pipewire = {
      enable = true;
      pulse.enable = true;
@@ -62,7 +50,6 @@
    services.libinput.enable = true;
 
   # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
    services.openssh.enable = true;
 
@@ -72,27 +59,7 @@
   # Or disable the firewall altogether.
    networking.firewall.enable = false;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-   # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
+  #DO NOT F***ING TOUCH OR CHANGE
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.05"; # Did you read the comment?
 
