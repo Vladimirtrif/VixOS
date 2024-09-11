efc2756e5a71fcc6dcb25b5e4860e372ad5e7207
Vladimirtrif <59485365+Vladimirtrif@users.noreply.github.com>
2024-09-11 21:15:51 +0300

    staged files

---
+++ nixos/modules/users.nix
@@ -0,0 +1,10 @@
+{
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+   users.users.vova = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" "docker" "vboxsf"]; # Enable ‘sudo’ for the user.
+     home = "/home/vova";
+     packages = with pkgs; [
+     ];
+   };
+}
