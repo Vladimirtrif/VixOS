028919f08c7fe5dff009f65cf17bd6221fee0692
Vladimirtrif <59485365+Vladimirtrif@users.noreply.github.com>
2024-09-22 09:07:27 -0700

    Update bootloader.nix

--- nixos/modules/bootloader.nix
+++ nixos/modules/bootloader.nix
@@ -1,5 +1,5 @@
 {
-     boot.loader.efi.canTouchEfiVariables = false
+     boot.loader.efi.canTouchEfiVariables = false;
      boot.loader.grub.enable = true;
      boot.loader.grub.efiSupport = true;
      boot.loader.grub.efiInstallAsRemovable = true;
