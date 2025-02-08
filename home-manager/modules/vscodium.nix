a77289bc93b8567fd0a7c9a8bb073a5440c05127
Vladimirtrif <vova.trifonov@hotmail.com>
2025-02-07 23:17:02 -0800

    Replaced stylix vscode with catppuccin extension

--- home-manager/modules/vscodium.nix
+++ home-manager/modules/vscodium.nix
@@ -1,9 +1,10 @@
 {pkgs, ...}: {
+    stylix.targets.vscode.enable = false;
     programs.vscode = {
         enable = true;
         package = pkgs.vscodium;
         extensions = with pkgs.vscode-extensions; [
-            
+            catppuccin.catppuccin-vsc
         ];
     };
 }
