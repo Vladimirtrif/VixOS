1adfa4343898a45d2d3b73f00e79433fcd9ece9e
Vladimirtrif <vova.trifonov@hotmail.com>
2025-04-21 00:58:53 -0700

    ags

---
+++ home-manager/modules/WM-Shell/ags/widget/TopBar.tsx
@@ -0,0 +1,21 @@
+import { App, Astal, Gtk, Gdk } from "astal/gtk4"
+import Time from "./bar modules/Time"
+import { Workspaces } from "./bar modules/Workspaces"
+
+export default function TopBar(gdkmonitor: Gdk.Monitor) {
+    const { TOP } = Astal.WindowAnchor
+
+    return <window
+        visible
+        cssClasses={["Bar"]}
+        gdkmonitor={gdkmonitor}
+        exclusivity={Astal.Exclusivity.EXCLUSIVE}
+        anchor={TOP}
+        application={App}>
+        <centerbox cssName="centerbox">
+            <box />
+            {Time}
+            {Workspaces}
+        </centerbox>
+    </window>
+}
