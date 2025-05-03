829406f03fe031f1a00f1708c4eaf9800cc64413
Vladimirtrif <vova.trifonov@hotmail.com>
2025-05-03 00:44:40 -0700

    small stuff

--- scripts/Rofi/Audio/main_audio.sh
+++ scripts/Rofi/Audio/main_audio.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-CHOSEN=$(printf "Volume +5%%\nVolume -5%%\nChange Devices" | rofi -dmenu -i)
+CHOSEN=$(printf "Volume +5%%\nVolume -5%%\nChange Devices\nAdvanced" | rofi -dmenu -i)
 
 case "$CHOSEN" in
 	"Volume +5%") wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+;;
     "Volume -5%") wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-;;
     "Change Devices") ./device_audio.sh;;
+    "Advanced") pavucontrol;;
 	
 	*) exit 1 ;;
 esac
