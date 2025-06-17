13a9786d759d2bbfc647358da40f09f87f2327f7
Vladimirtrif <vova.trifonov@hotmail.com>
2025-06-17 01:07:11 -0700

    Vscode config

--- home-manager/modules/vscodium.nix
+++ home-manager/modules/vscodium.nix
@@ -5,24 +5,30 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-     profiles.default.userSettings = {
-      "editor.fontFamily" = "'${config.stylix.fonts.monospace.name}'";
-      "editor.fontLigatures" = "true";
-      "terminal.integrated.fontLigatures.enabled" = true;
-      "editor.fontSize" = (config.stylix.fonts.sizes.terminal + 2);
-      "window.zoomLevel" = 0.25;
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        ms-vscode-remote.remote-ssh
+        ms-vsliveshare.vsliveshare
+      ];
+      userSettings = {
+        "editor.fontFamily" = "'${config.stylix.fonts.monospace.name}'";
+        "editor.fontLigatures" = "true";
+        "terminal.integrated.fontLigatures.enabled" = true;
+        "editor.fontSize" = (config.stylix.fonts.sizes.terminal + 2);
+        "window.zoomLevel" = 0.25;
 
-      # for catppuccin
-      # we try to make semantic highlighting look good
-      "editor.semanticHighlighting.enabled" = true;
-      # prevent VSCode from modifying the terminal colors
-      "terminal.integrated.minimumContrastRatio" = 1;
-      # make the window's titlebar use the workbench colors
-      "window.titleBarStyle" = "custom";
-      # applicable if you use Go, this is an opt-in flag!
-      "gopls" = {
-        "ui.semanticTokens" = true;
+        # for catppuccin
+        # we try to make semantic highlighting look good
+        "editor.semanticHighlighting.enabled" = true;
+        # prevent VSCode from modifying the terminal colors
+        "terminal.integrated.minimumContrastRatio" = 1;
+        # make the window's titlebar use the workbench colors
+        "window.titleBarStyle" = "custom";
+        # applicable if you use Go, this is an opt-in flag!
+        "gopls" = {
+          "ui.semanticTokens" = true;
         };
-     };
+      };
+    };
   }; 
 }
