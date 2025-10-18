42673bfe4f5196c1275acae58c0a8f3b41b63948
Vladimirtrif <vova.trifonov@hotmail.com>
2025-10-18 16:14:37 -0700

    added bat info to sysinfo script

--- scripts/consts.nix
+++ scripts/consts.nix
@@ -1,3 +1,14 @@
+let
+  bat_dir = ''/sys/class/power_supply/BAT0'';
+in
+let
+  bat_info = ''
+    $(if [ -d ${bat_dir} ]; then
+        printf %b "\n Battery: $(cat ${bat_dir}/capacity)%, $(cat ${bat_dir}/status)"
+      fi)
+  '';
+in
+
 {
-  INFO = ''$(printf '%b' "Time: $(date +"%I:%M %p")\nDate: $(date +"%B %d, %Y")")'';
+  INFO = ''$(printf '%b' "Time: $(date +"%I:%M %p")\nDate: $(date +"%B %d, %Y")${bat_info}")'';
 }
