214b4eea9d3467bb7c70e81d5ef9b6a8ecda78fd
Vladimirtrif <vova.trifonov@hotmail.com>
2025-11-28 00:54:01 -0800

    mango conf

--- home/modules/WM-Shell/mango.nix
+++ home/modules/WM-Shell/mango.nix
@@ -10,6 +10,7 @@ let
   terminal = "kitty";
   browser = "zen";
   email = "thunderbird";
+  editor = "nvim";
 
 in
 {
@@ -17,27 +18,32 @@ in
     enable = true;
     settings = ''
       # Keybinds
-
-      # Two modes, default (navigation) and editing
       keymode=common
       bind=SUPER,Escape,setkeymode,default
       bind=SUPER,e,setkeymode,editing
 
+
+      # Two modes, default (navigation) and editing
       keymode=default
-      bind=SUPER,t,spawn,${terminal}
-      bind=SUPER,b,spawn, ${browser}
-      bind=SUPER,m,spawn, ${email}
-      bind=SUPER,r,spawn, ${menu}
+
       bind=SUPER,h,focusdir,left
       bind=SUPER,j,focusdir,down
       bind=SUPER,k,focusdir,up
       bind=SUPER,l,focusdir,right
 
+      bind=SUPER,t,spawn,${terminal}
+      bind=SUPER,b,spawn, ${browser}
+      bind=SUPER,m,spawn, ${email}
+      bind=SUPER,r,spawn, ${menu}
+      bind=SUPER,c,spawn_shell, ${editor}
+
       bind=SUPER,f,togglefullscreen
-      bind=SUPER,c,killclient
+      bind=SUPER,q,killclient
 
       bind=SUPER,s,viewtoleft
       bind=SUPER,d,viewtoright
+      bind=SUPER+SHIFT,s,tagtoleft
+      bind=SUPER+SHIFT,d,tagtoright
 
       bind=SUPER,1,view,1
       bind=SUPER,2,view,2
@@ -50,16 +56,35 @@ in
       bind=SUPER,9,view,9
       bind=SUPER,0,view,10
 
+      bind=SUPER+SHIFT,1,tag,1
+      bind=SUPER+SHIFT,2,tag,2
+      bind=SUPER+SHIFT,3,tag,3
+      bind=SUPER+SHIFT,4,tag,4
+      bind=SUPER+SHIFT,5,tag,5
+      bind=SUPER+SHIFT,6,tag,6
+      bind=SUPER+SHIFT,7,tag,7
+      bind=SUPER+SHIFT,8,tag,8
+      bind=SUPER+SHIFT,9,tag,9
+      bind=SUPER+SHIFT,0,tag,10
+
+
       keymode=editing
+
       bind=SUPER,h,exchange_client,left
       bind=SUPER,j,exchange_client,down
       bind=SUPER,k,exchange_client,up
       bind=SUPER,l,exchange_client,right
 
+      bind=SUPER+SHIFT,h,focusdir,left
+      bind=SUPER+SHIFT,j,focusdir,down
+      bind=SUPER+SHIFT,k,focusdir,up
+      bind=SUPER+SHIFT,l,focusdir,right
+
       bind=SUPER,s,resizewin,-50,+0 #left
       bind=SUPER,f,resizewin,+0,+50 #down
       bind=SUPER,d,resizewin,+0,-50 #up
       bind=SUPER,g,resizewin,+50,+0 #right
+
 
       # set all tags to tile by default
       tagrule=id:1,layout_name:tile
